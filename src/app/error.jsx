e83c89f462e7eec85c1f26b8352fb60c69d0e0ab
Sogon-Njugush <sogon5045@gmail.com>
2024-04-06 01:26:31 +0300

    adjustment for vercel deployment

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,10 +1,10 @@
 'use client'
-import { useEffect } from 'react'
+// import { useEffect } from 'react'
 
 export default function error({error, reset}) {
-    useEffect(()=>{
-        console.log(error);
-    }, [error])
+    // useEffect(()=>{
+    //     // console.log(error);
+    // }, [error])
   return (
     <div className='text-center mt-20'>
         <h1>Something went wrong. Please try again</h1>
