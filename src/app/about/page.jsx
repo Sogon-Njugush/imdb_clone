7f7a456f65777c2fd2d83daa174ce533eaea9b2f
Sogon-Njugush <sogon5045@gmail.com>
2024-04-05 20:32:35 +0300

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-500'>About</h1>
+        <p>
+        Nulla venenatis lorem sed ipsum consectetur facilisis. Sed condimentum lacus blandit maximus blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris mattis vestibulum ex id condimentum. Vestibulum dignissim tristique posuere. Pellentesque lobortis, arcu quis fermentum feugiat, elit eros mattis lacus, a mattis risus quam in ligula. Cras cursus fringilla mauris vitae finibus. Aenean id tortor sit amet lectus viverra malesuada in vitae lectus. Curabitur pretium sagittis nibh, nec ornare nulla placerat vel. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi nibh lacus, cursus sed erat placerat, auctor finibus diam. Curabitur in rutrum justo. In hac habitasse platea dictumst. Proin dapibus molestie purus quis sollicitudin.
+        </p>
+        <p>
+        Donec ultricies, ex vel efficitur feugiat, orci ex hendrerit risus, tincidunt suscipit nibh est vitae tellus. Pellentesque iaculis pharetra sodales. Aliquam erat volutpat. Nulla facilisi. Etiam vitae elementum nisi. Duis venenatis eu felis ac porta. Donec fermentum turpis id est ullamcorper tempus. Suspendisse leo urna, euismod sagittis mattis sed, bibendum sed sem. Donec feugiat vel elit ut sagittis. Sed vulputate porta odio eu ultricies. Maecenas bibendum pretium sapien eu finibus.
+        </p>
+    </div>
+  )
+}
