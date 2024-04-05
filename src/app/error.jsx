3050a4d9af9fc76407a27c1b90afa754d9bc41fc
Sogon-Njugush <sogon5045@gmail.com>
2024-04-06 01:29:51 +0300

    error page update

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,10 +1,11 @@
 'use client'
-// import { useEffect } from 'react'
 
-export default function error({error, reset}) {
-    // useEffect(()=>{
-    //     // console.log(error);
-    // }, [error])
+import { useEffect } from 'react'
+
+export default function Error({error, reset}) {
+    useEffect(()=>{
+        console.log(error);
+    }, [error])
   return (
     <div className='text-center mt-20'>
         <h1>Something went wrong. Please try again</h1>
